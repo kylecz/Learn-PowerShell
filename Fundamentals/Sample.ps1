ce709c945fcd48434cdf1fc2b8c058034a48ae46
Kyle Czernuszka <czernuka@outlook.com>
2021-12-22 14:26:16 -0500

    commit

--- Fundamentals/Sample.ps1
+++ Fundamentals/Sample.ps1
@@ -1,23 +1,25 @@
-## Module 1 - Fundamentals
+#### Module 1
 
 dir | sort-object
 dir | sort-object -descending
 dir | sort-object -descending -property lastwritetime
 
-Get-ChildItem
-
 $PSVersionTable
 
 Get-Module -listavailable
 
+Get-Host
+$PSVersionTable
+$PSVersionTable.PSVersion
+
+Dir
+Get-ChildItem
+Get-Alias Dir
+
+
 Write-Output "Hello"
 $name = "Kyle"
 Write-Output $name
-
-## Module 2
-
-
-
 
 
 
